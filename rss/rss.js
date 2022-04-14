952b7f0f1efc44737375c04ab76f6de962bf9d3b
Guerinlupus <74427691+guerinlupus@users.noreply.github.com>
2022-04-14 12:32:03 +0200

    Delete rss.js

--- rss/rss.js
+++
@@ -1,7 +0,0 @@
-var container = document.createElement(div);
-container.innerHTML = h3 class=feed-titleimg class=feed-title-image src=httpster-hdf-p44.netlify.apprssicon.jpga href=httpster-hdf-p44.netlify.appinfo_traffic.html target=_blank&lt;h1&gt;Info Traffic&lt;h1&gt;ah3h4 class=feed-item-titlea href=httpster-hdf-p44.netlify.apppost_info-trafficport_du_masque.html target=_blankPort du Masque &gt;&gt;ah4p class=feed-item-desci aria-hidden=true class=icons-circle-information icons-size-1x25inpLe port du masque reste strongobligatoire à partir de 6 ansstrong à bord de tous les trains ainsi que dans les zones où le titre de transport est obligatoire. Le masque est obligatoire en gare au-delà du dispositif de contrôle du titre de transport (portique d'accès aux quais, bornes de validation ou contrôle à l'embarquement).pnhrp;
-var nodes = [].slice.call(container.childNodes);
-var script = document.scripts[document.scripts.length - 1];
-var parent = script.parentNode;
-while(nodes.length  0) parent.insertBefore(nodes.shift(), script);
-parent.removeChild(script);
